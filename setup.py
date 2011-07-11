aad59554b3bfda60807d525f0868f022414e7406
Dan Crosta <dcrosta@gmail.com>
2011-07-11 14:27:55 -0400

    basic dump/restore tool with command line support

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymongodump',
+    version='0.1',
+    description='utility to dump and restore mongodb databases from python (with pymongo)',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    author='Daniel Crosta',
+    author_email='dcrosta@late.am',
+    url='',
+    keywords='mongodb dump restore pymongo',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pymongo'],
+)
+
